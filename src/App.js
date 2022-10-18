971dd4c31f2c30365b913fe4849e6c0d7878a82b
bwsrcurtis <bwsrcurtis@gmail.com>
2022-10-18 19:13:56 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
 import React from 'react';
 
-import maryland from './static/images/maryland.png'
 import './App.css';
+import maryland from './static/images/maryland.png'
 import MasonryImageList from './components/imageList';
 
 function App() {
